042a17ff752c15b9bbd5e19db4750a825708dae5
Joao-GabrielPS <joao22.souza@alunos.ifsuldeminas.edu.br>
2025-04-02 11:36:45 -0300

    atividade_0204

---
+++ InfoE2_Joao_Gabriel/src/atividade_0204/Aluno.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade_0204;
+
+/**
+ *
+ * @author 12409864678
+ */
+public class Aluno {
+    
+}
--- InfoE2_Joao_Gabriel/src/banco/Conta.java
+++ InfoE2_Joao_Gabriel/src/banco/Conta.java
@@ -10,5 +10,33 @@ package banco;
  * @author 12409864678
  */
 public class Conta {
+    int numero; //atributos ou variáveis de instância
+    String cliente;
+    double saldo;
+    double limite;
+    double transfere;
     
+    public void saca (double quantidade) { //método
+        if(quantidade > this.saldo){
+        System.out.println("Prezado "+this.cliente+" .SaldoInsuficiente. Seu saldo é: "+this.saldo);
+        }
+        else{
+        this.saldo = this.saldo - quantidade;
+        }
+        }
+    public void deposita (double quantidade){
+        this.saldo = this.saldo + quantidade;
+    }
+    
+    public void transfere (Conta destino, double valor ){
+        if(valor>this.saldo){
+        this.saldo = this.saldo - valor;
+        System.out.println("Saldo insuficiente para transferência");
+    }else{
+    //destino.saldo = destino.saldo - valor // saque
+    this.saldo = this.saldo - valor;
+    //destino.saldo = destino.saldo + valor // deposito
+    destino.deposita(valor);
+}
+    }
 }
